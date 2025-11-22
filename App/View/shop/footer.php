6ba64a6871ecaf72dd36d3bb21fcc32fce0ff1a9
tronghoainguyen5-collab <tronghoainguyen5@gmail.com>
2025-11-22 10:58:23 +0700

    Update footer.php

---
+++ App/View/shop/footer.php
@@ -0,0 +1,80 @@
+<footer class="footer">
+
+    <!-- Đăng ký nhận tin -->
+    <div class="footer-news">
+        <div class="news-left">
+            <h3>ĐĂNG KÝ NHẬN THÔNG TIN TỪ 5SV</h3>
+            <p>Đừng bỏ lỡ các chương trình khuyến mãi mới, hấp dẫn</p>
+
+            <div class="newsletter-box">
+                <input type="email" placeholder="Nhập Email">
+                <button><i class="fa-solid fa-paper-plane"></i></button>
+            </div>
+        </div>
+
+        <div class="news-right">
+            <p><i class="fa-solid fa-phone"></i> 1900 9201</p>
+            <p><i class="fa-solid fa-envelope"></i> cskh@5sv.vn</p>
+</div>
+            <div class="social-links">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-facebook-messenger"></i>
+                <i class="fa-brands fa-tiktok"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+        
+    </div>
+
+    <hr class="footer-line">
+
+    <!-- Thông tin footer -->
+    <div class="footer-content">
+
+        <div class="footer-col">
+            <h4>DELTAHOLIC</h4>
+            <a href="#">Đăng ký thành viên</a>
+            <a href="#">Ưu đãi & Đặc quyền</a>
+        </div>
+
+        <div class="footer-col">
+            <h4>MUA SẮM</h4>
+            <a href="#">Hỏi đáp - FAQs</a>
+            <a href="#">Hướng dẫn mua hàng & sử dụng voucher</a>
+            <a href="#">Hướng dẫn chọn size</a>
+            <a href="#">Hướng dẫn thanh toán VNPAY</a>
+            <a href="#">In ấn đội nhóm TEAMPRINT</a>
+        </div>
+
+        <div class="footer-col">
+            <h4>CHÍNH SÁCH</h4>
+            <a href="#">Chính sách đổi trả & bảo hành</a>
+            <a href="#">Chính sách giao hàng</a>
+            <a href="#">Chính sách khuyến mãi</a>
+            <a href="#">Chính sách bảo mật</a>
+        </div>
+
+        <div class="footer-col">
+            <h4>VỀ DELTA</h4>
+            <a href="#">Giới thiệu</a>
+            <a href="#">Cam kết thương hiệu</a>
+            <a href="#">Hệ thống cửa hàng</a>
+            <a href="#">Tuyển dụng</a>
+        </div>
+
+        <div class="footer-col">
+            <h4>ĐỊA CHỈ LIÊN HỆ</h4>
+            <p>Chi nhánh HCM: Số 1, Đường B, Khu ADC, Phường Phú Thạnh, Quận Tân Phú, Thành phố Hồ Chí Minh, Việt Nam</p>
+            <br>
+            <p>Head Office: Phố Trung Sơn, Tỉnh lộ 510,<br>Thị Trấn Bút Sơn, Huyện Hoằng Hoá, Tỉnh Thanh Hoá, Việt Nam</p>
+        </div>
+    </div>
+
+    <hr class="footer-line">
+
+    <!-- Copyright -->
+    <div class="footer-bottom">
+        <p>Số ĐKKD: 2800702548 do sở Kế Hoạch và Đầu Tư tỉnh Thanh Hóa cấp lần đầu ngày 05/08/2002</p>
+        <img src="App/img/logo-da-thong-bao-bo-cong-thuong-300x114.webp" alt="Bộ Công Thương">
+    </div>
+
+</footer>
