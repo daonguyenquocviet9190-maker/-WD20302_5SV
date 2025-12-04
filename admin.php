fb9c2523408b4e75d6dbe306aabbddf278e34d9c
tronghoainguyen5-collab <tronghoainguyen5@gmail.com>
2025-12-04 11:05:03 +0700

    admin

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 include "App/Controllers/AdminController.php";
     $controller = new AdminController();
-include "App/View/admin/header.php";
+include_once "App/View/admin/header.php";
 
 if (!isset($_GET['page'])){
        header('location:admin.php?page=home');
